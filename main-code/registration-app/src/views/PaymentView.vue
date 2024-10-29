34c57786ce3444d2626644852298ff2bc60a2f5e
therealirfanshafi <thatirfanshafi@gmail.com>
2024-10-29 23:58:44 +0600

    Built frontend for production

--- main-code/registration-app/src/views/PaymentView.vue
+++ main-code/registration-app/src/views/PaymentView.vue
@@ -1,10 +1,19 @@
 <template>
   <main>
     <div :class="{ blue: category == 'Junior', red: category == 'Senior' }">
-      <h1 style="text-align: center">
+      <h1>
         Complete Payment by clicking the button below. The registration is fee is 2000 tk. Once paid
-        your spot in the fest will be confirmed
+        your spot in the fest will be confirmed.
       </h1>
+      <h1>
+        Once you start payment, you need to complete it in 15 min, otherwise you have to start
+        over.If payment is succesful, you will be sent a payment confirmation mail.
+      </h1>
+      <h1>
+        You can also pay physically or through your campus ambassador in which case you will be sent
+        a confirmation mail once we verify your payment.
+      </h1>
+
       <button
         :class="{ blue: category == 'Junior', red: category == 'Senior' }"
         @click="completePayment()"
@@ -61,6 +70,13 @@ main {
   justify-content: center;
 }
 
+h1 {
+  font-size: 1.5rem;
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
 div {
   width: 80%;
   padding: 40px;
@@ -81,5 +97,11 @@ button {
 button:hover,
 button:focus {
   cursor: pointer;
+}
+
+@media screen and (min-width: 600px) {
+  h1 {
+    font-size: 2rem;
+  }
 }
 </style>
